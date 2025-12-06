9de7d0a31a4c4af25614c7b722a2bdeb816a48c4
Puneendra <samanapuneendra@gmail.com>
2025-12-06 08:30:31 +0530

    first commit

---
+++ src/test/java/StepDefinition/StepDefinition.java
@@ -0,0 +1,31 @@
+package StepDefinition;
+
+import io.cucumber.java.en.*;
+
+public class StepDefinition
+{
+    @Given("create api request for adding a location")
+    public void create_api_request_for_adding_a_location() {
+
+    }
+    @When("Do post call with query peramaters")
+    public void do_post_call_with_query_peramaters() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("validate the status code as {int}")
+    public void validate_the_status_code_as(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("Verify status as OK")
+    public void verify_status_as_ok() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("also verify scope as APP")
+    public void also_verify_scope_as_app() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
