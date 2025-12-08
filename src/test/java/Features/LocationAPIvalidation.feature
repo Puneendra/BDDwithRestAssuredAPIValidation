5895bb26e4f9d1cca270e1c4acf90c5ff8db7aeb
Puneendra <samanapuneendra@gmail.com>
2025-12-08 17:05:06 +0530

    work on first featuer

--- src/test/java/Features/LocationAPIvalidation.feature
+++ src/test/java/Features/LocationAPIvalidation.feature
@@ -3,6 +3,5 @@ Feature: Location API Validation
   Scenario: Verify the location is added successfully by using post call in API
     Given create api request for adding a location
     When Do post call with query peramaters
-    Then validate the status code as 200
-    And Verify status as OK
+    Then Verify status as OK
     And also verify scope as APP
--- src/test/java/StepDefinition/StepDefinition.java
+++ src/test/java/StepDefinition/StepDefinition.java
@@ -1,31 +1,110 @@
 package StepDefinition;
 
+import PojoClasess.AddLocation;
+import PojoClasess.LocationDetails;
 import io.cucumber.java.en.*;
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.junit.Assert;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class StepDefinition
 {
-    @Given("create api request for adding a location")
-    public void create_api_request_for_adding_a_location() {
+    ResponseSpecification responseaobj;
+    RequestSpecification aobj;
+    JsonPath js;
 
+    @Given("create api request for adding a location")
+    public void creatingApiRequest()
+    {
+        RestAssured.baseURI = "https://rahulshettyacademy.com";
+        AddLocation location1 = new AddLocation();
+        LocationDetails locationaobj = new LocationDetails();
+
+        locationaobj.setLat(-38.383494);
+        locationaobj.setLng(33.427362);
+        location1.setLocation(locationaobj);
+
+
+        location1.setAccuracy(50);
+        location1.setName("Puneendra");
+        location1.setPhone_number("7337238146");
+        location1.setAddress("Puneendra from hyderabad");
+
+
+        List<String> types = new ArrayList<>();
+        types.add("Temple");
+        types.add("Naidu street ");
+        location1.setTypes(types);
+
+        location1.setWebsite("http://google.com");
+        location1.setLanguage("Telugu");
+
+        System.out.println(location1);
+
+         aobj = new RequestSpecBuilder()
+                .setBaseUri("https://rahulshettyacademy.com")
+                .addQueryParam("key", "qaclick123")
+                .setContentType(ContentType.JSON).build();
+
+
+         responseaobj = new ResponseSpecBuilder()
+                .expectStatusCode(200)
+                .expectContentType(ContentType.JSON).build();
+
+
+        String responseResult =responseaobj
+
+
+                .when()
+                .post("/maps/api/place/add/json")
+
+                .then().spec(responseaobj)
+                .log().all().extract().asString();
     }
     @When("Do post call with query peramaters")
-    public void do_post_call_with_query_peramaters() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void CallingAPI() {
+        String responseResult =responseaobj
+
+
+                .when()
+                .post("/maps/api/place/add/json")
+
+                .then().spec(responseaobj)
+                .log().all().extract().asString();
+        System.out.println("Response: " + responseResult);
+
+         js = new JsonPath(responseResult);
     }
-    @Then("validate the status code as {int}")
-    public void validate_the_status_code_as(Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
+
+
     @Then("Verify status as OK")
-    public void verify_status_as_ok() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void verify_status_as_ok()
+    {
+//        String id = js.getString("id");
+//        String placeid = js.getString("place_id");
+//
+//        System.out.println("ID: " + id);
+//        System.out.println("Place ID: " + placeid);
+
+String ActualStatus = js.getString("status");
+        Assert.assertEquals(ActualStatus,"OK","Status code is not correct in respone code");
+
     }
-    @Then("also verify scope as APP")
+    @And("also verify scope as APP")
     public void also_verify_scope_as_app() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
+        String ActualScope = js.getString("scope");
+        Assert.assertEquals(ActualScope,"APP","Scope is not correct in respone code");
+
+
+
     }
 }
